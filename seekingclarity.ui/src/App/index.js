e5c13aa5a3085dcf5f55a26c89b1a4ef0005a7c1
Wendell Patton Jr <wendell.pattonjr@gmail.com>
2021-05-29 00:49:36 -0500

    corrected some issues with front iend react setup

--- seekingclarity.ui/src/App/index.js
+++ seekingclarity.ui/src/App/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='App'>
-      <h2>SeekingClarity App</h2>
+      <h2>Seeking Clarity App</h2>
       <button className='btn btn-info'>I am a button</button>
     </div>
   );
